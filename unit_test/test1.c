3a113494619a3ffac17f53816ddb94be5cfe58ba
sexybug <hello.bug@outlook.com>
2025-07-30 09:52:08 +0800

    add hex_to_int8 test

--- unit_test/test1.c
+++ unit_test/test1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "cc_test.h"
 
 uint32_t mbedtls_mpi_core_montmul_init(const uint32_t *N)
 {
@@ -19,27 +20,39 @@ uint32_t mbedtls_mpi_core_montmul_init(const uint32_t *N)
 
 int main()
 {
+    // {
+    //     uint32_t v = 0x80000000;
+    //     v = v >> 1;
+
+    //     uint32_t a = 0xf0000001 % 3;
+
+    //     uint32_t N[2] = {0x87654321, 0x87654321};
+    //     uint32_t N_inv = mbedtls_mpi_core_montmul_init(N);
+    // }
+
+    // {
+    //     uint64_t a = 1 << 31;
+    //     uint64_t b = (uint64_t)1 << 32;
+
+    //     printf("%llx\n", a);
+    //     printf("%llx\n", b);
+    // }
+    // {
+    //     size_t a = 0xFFFFFFFF;
+    //     int b = a;
+    //     printf("%d\n", b);
+    // }
+
     {
-        uint32_t v = 0x80000000;
-        v = v >> 1;
-
-        uint32_t a = 0xf0000001 % 3;
-
-        uint32_t N[2] = {0x87654321, 0x87654321};
-        uint32_t N_inv = mbedtls_mpi_core_montmul_init(N);
-    }
-
-    {
-        uint64_t a = 1 << 31;
-        uint64_t b = (uint64_t)1 << 32;
-
-        printf("%llx\n", a);
-        printf("%llx\n", b);
-    }
-    {
-        size_t a = 0xFFFFFFFF;
-        int b = a;
-        printf("%d\n", b);
+        char *str = "FFFE0102";
+        int8_t dst[300];
+        int len = strlen(str)/2;
+        HexString2Hex(str, len, dst);
+        int i;
+        for(i=0;i<len;i++)
+        {
+            printf("%d,",dst[i]);
+        }
     }
 
     return 0;
